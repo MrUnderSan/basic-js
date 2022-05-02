0d619b07be4bdf1ead828ba5ca9674e9497daa18
MrUnderSan <MrUnderSan@gmail.com>
2022-05-02 18:34:22 +0300

    1

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,38 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (Array.isArray(members) === false || members.length === 0) {
+    return false;
+  }
+  let nameArray = [];
+  members.forEach((elem) => {
+    if (typeof elem === "string") {
+      for (let i = 0; i < elem.length; i++) {
+        if (elem[i] != " ") {
+          nameArray.push(elem[i].toUpperCase());
+          break;
+        }
+      }
+    }
+  });
+  return nameArray.sort().join("");
 }
 
+console.log(createDreamTeam());
+
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,35 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (date != true) {
+    return "Unable to determine the time of year!";
+  } else if (
+    (date.getMonth() >= 0 && date.getMonth() < 2) ||
+    date.getMonth() == 11
+  ) {
+    return "winter";
+  } else if (date.getMonth() >= 2 && date.getMonth() < 5) {
+    return "spring";
+  } else if (date.getMonth() >= 5 && date.getMonth() < 8) {
+    return "summer ";
+  } else if (date.getMonth() >= 8 && date.getMonth() < 11) {
+    return "autumn ";
+  } else {
+    return "Invalid date!";
+  }
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
